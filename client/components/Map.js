8376fe5fd5f68dd2f1f35e5f580dd0c41db3b7c5
tiffanyip <tiffanyip@ucla.edu>
2016-11-11 08:06:26 -0800

    remove sample data

--- client/components/Map.js
+++ client/components/Map.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchMovies } from '../actions/index';
-import sample from '../sampleData';
 
 let map;
 let markerData;
